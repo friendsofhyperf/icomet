69df35bc84fae920ca17788bb370053e9ca51a58
huangdijia <huangdijia@gmail.com>
2022-07-12 14:56:01 +0800

    format

--- publish/icomet.php
+++ publish/icomet.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * This file is part of icomet.
  *
  * @link     https://github.com/friendsofhyperf/icomet
- * @document https://github.com/friendsofhyperf/icomet/blob/main/README.md
+ * @document https://github.com/friendsofhyperf/icomet/blob/0.x/README.md
  * @contact  huangdijia@gmail.com
  */
 return [
